64f9656e6832b33625d14d805757833393956772
sydarthian <lillydonthe7@gmail.com>
2025-04-21 21:08:23 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Rock_Paper_Scissors
+// Author: Syd Lyle
+// Date: April 2025
 
 // Constants
 
@@ -19,3 +19,18 @@ function main() {
 
 // let's get this party started
 main();
+
+//Display game instructions
+//Get player input 
+   // Prompt the player to choose rock, paper, or scissors.
+//Generate computer choice 
+   // Randomly select rock, paper, or scissors for the computer.
+//Determine the winner 
+   //Compare the player’s choice and the computer’s choice. Apply the rules (rock beats scissors, etc.).
+//Display the result 
+   //Show what both sides chose and who won.
+//Ask to play again
+   //Allow the player to decide whether to play another round or exit.
+//Loop or exit
+   // If the player wants to play again, repeat from step 2.
+   //  Otherwise, end the game with a closing message.
