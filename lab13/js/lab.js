97dc5525bb3546711537666ea01cfcf9b04a7e1e
sydarthian <lillydonthe7@gmail.com>
2025-05-25 12:38:14 -0700

    Update lab.js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -7,9 +7,7 @@ maxFactors = 4;
 outputEl = document.getElementById("output");
 
 // get the values from the webpage and write them in an object
-// this expects to have input fields with ids num0, text0, num1, text1, etc
-// returns an object that looks like this:
-//      {3: "Fizz", 5: "Buzz", 7: "Boom"}
+
 function getFactorObj() {
     var factorObj = {};
     for (var factor=0; factor<maxFactors; factor++) {
@@ -32,17 +30,14 @@ function outputToPage(str) {
     outputEl.appendChild(newEl);
 }
 
-// given a number and an object that looks like this:
-//      {3: "Fizz", 5: "Buzz", 7: "Boom"}
 // loops over the numbers and outputs the number and the matching text
-// for factors
 function fizzBuzzBoom(maxNums, factorObj) {
-    // iterate over all of out numbers
+
     for (var num=0; num<maxNums; num++) {
         debugger;
-        // reset output string
+        
         var outputStr = "";
-        // iterate over the factors we got from the html
+       
         for (var factor in factorObj) {
             // check to see if this num is a multiple of factor
             if (num % factor == 0) {
@@ -50,7 +45,7 @@ function fizzBuzzBoom(maxNums, factorObj) {
                 outputStr += factorObj[factor];
             }
         }
-        // now if we have words in outputStr, format it like this " - FizzBuzz!"
+        
         if (outputStr) {
             outputStr = " - " + outputStr + "!";
         }
