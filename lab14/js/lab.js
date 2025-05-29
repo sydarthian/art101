a716be1d47509792e7ba42462fef11b937bfa4f2
sydarthian <lillydonthe7@gmail.com>
2025-05-29 10:28:29 -0700

    lab14

---
+++ lab14/js/lab.js
@@ -0,0 +1,10 @@
+// index.js - purpose and description here
+// Author: Syd Lyle
+// Date:
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+main();
